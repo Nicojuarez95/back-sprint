a4daa1dd47d50048cfb1af6a181833bae2084c50
Nico <juareznico61@gmail.com>
2023-02-23 16:49:09 -0300

    base de datos users ok

---
+++ models/data/users.js
@@ -0,0 +1,7 @@
+import User from "../User.js";
+import 'dotenv/config.js'
+import '../../config/database.js'
+
+let users = []
+
+User.insertMany(users)
---
+++ routes/index.js
@@ -0,0 +1,15 @@
+import userRouter from './users.js'
+
+import express from 'express'
+let router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+//rutas de todos los recursos
+//a traves del metodo .use() le indico al enrutador principal que use esas rutas con esa palabrita(endpoint)
+router.use('/users',userRouter)
+
+export default router
