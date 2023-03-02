1f4e45a710917164a25b1762092c431c12c3b567
Nico <juareznico61@gmail.com>
2023-03-02 17:53:19 -0300

    back user listo

--- schemas/users.js
+++ schemas/users.js
@@ -10,12 +10,21 @@ const schema = Joi.object({
     "string.min": "The name must have at least 3 characteres",
     "string.max": "The name must have a maximum of 20 characteres",
   }),
-  email: Joi.string()
+  email: Joi
+    .string()
     .required()
     .min(8)
     .email({ minDomainSegments: 2 })
     .messages({
     invalid: "Not an objets",
+    }),
+  photo: Joi.string()
+    .uri()
+    .required()
+    .min(8)
+    .messages({
+      "string.required": "photo required",
+      "uri.required": "url required"
     }),
   password: Joi
     .string()
