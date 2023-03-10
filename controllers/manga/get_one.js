5ea92567846c68aca4385a5aa38c8676ec986d54
salomon.cjl <contrerassalomon6@gmail.com>
2023-03-10 13:18:43 -0300

    M-11 get_one

---
+++ controllers/manga/get_one.js
@@ -0,0 +1,17 @@
+import Manga from '../../models/Manga.js'
+
+const controller = {
+    getOne: async (req, res) => {
+        const manga = await Manga.findOne({ _id: req.params.id }).select('title description -_id')
+        if(manga){
+            return res.status(200).json({category: manga})
+        } else {
+            return res.status(400).json({
+                success: false,
+                message: "Este manga no existe"
+            })
+        }
+    }
+}
+
+export default controller
