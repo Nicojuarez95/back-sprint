327abde16905e814874e50cb33f1ebb9b4859bfa
salomon.cjl <contrerassalomon6@gmail.com>
2023-03-11 00:23:02 -0300

    get_chapter echo... verlo por si acaso

---
+++ controllers/chapters/get_chapters.js
@@ -0,0 +1,20 @@
+import {Chapter} from '../../models/Chapter.js'
+
+async function get_chapter(req, res, next){
+    const manga_id = req.quey.manga_id;
+    const page = req.query.page || 1;
+    const limit = 4;
+    const skip = (page - 1) * limit;
+    try{
+        const chapter = await Chapter.find({manga_id})
+            .select('')
+            .sort({order: 1})
+            .skip(skip)
+            .limit(limit);
+        res.status(200).json({chapter});
+    } catch (error) {
+        next(error);
+    }
+}
+
+export default {get_chapter};
