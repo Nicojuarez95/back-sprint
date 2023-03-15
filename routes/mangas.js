6a958c3ed0d636531abdf34395e27e679913f535
salomon.cjl <contrerassalomon6@gmail.com>
2023-03-14 23:20:22 -0300

    Back temrinado

--- routes/mangas.js
+++ routes/mangas.js
@@ -7,15 +7,18 @@ import exist_title from '../middlewares/manga/exists_title.js'
 import is_active from '../middlewares/author/is_active.js'
 import getMangas from '../controllers/manga/get_mangas.js'
 import jwtmiddleware from '../middlewares/jwtmiddleware.js'
+import getOne from '../controllers/manga/get_one.js'
 
 let router = express.Router()
 const { create } = mangaCreate
 const { all } = allControllers
 const { allMangas } = getMangas
+const { get_one } = getOne
 
 
 router.post("/", jwtmiddleware ,is_active, validator(mangaSchema), exist_title, create);
 router.get("/", all);
 router.get("/view", allMangas)
+router.get('/:id', get_one )
 
 export default router
