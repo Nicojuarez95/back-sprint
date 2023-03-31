facdbda3524932c575eff45718ab75cbc2ad21c1
salomon.cjl <contrerassalomon6@gmail.com>
2023-03-30 22:53:17 -0300

    update terminado

--- routes/comments.js
+++ routes/comments.js
@@ -4,15 +4,18 @@ import createComment from '../controllers/comments/create.js'
 import all_from_chapters from '../controllers/comments/all_from_chapter.js'
 import validator from '../middlewares/validator.js'
 import passport from '../middlewares/passport.js'
+import updateComment from '../controllers/comments/update.js'
+import is_property_of from '../middlewares/comments/is_property_of.js'
 
 
 let router = express.Router();
 
 const { create } = createComment;
 const { all_from_chapter } = all_from_chapters
+const { update } = updateComment
 
 router.post('/', passport.authenticate("jwt", {session: false}), validator(createSchema), create);
 router.get('/', passport.authenticate("jwt", {session: false}), all_from_chapter);
-
+router.put('/:id', passport.authenticate("jwt", {session: false}), validator(createSchema), is_property_of, update)
 
 export default router
