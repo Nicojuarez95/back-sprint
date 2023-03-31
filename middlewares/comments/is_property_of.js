216d446fcaed48cf2978a75654eaf3dbe5ce1642
salomoncjl <115399253+salomoncjl@users.noreply.github.com>
2023-03-30 23:32:53 -0300

    M 16 dev (#19)
    * create comment
    
    * comments agregado a index router
    
    * Crear comentarios terminado
    
    * Read de comentarios y enrutado
    
    * update terminado

---
+++ middlewares/comments/is_property_of.js
@@ -0,0 +1,20 @@
+import { Comment } from "../../models/Comment.js";
+
+const is_property_of = async (req, res, next) => {
+    
+    try {
+        const comment = await Comment.findOne({ _id: req.params.id, user_id: req.user._id })
+        if (comment) {
+            return next()
+        } else {
+            return res.status(404).json({
+                message: 'You are not the owner of the comment'
+            })
+        }
+    } catch (error) {
+        return res.status(500).json({
+            message: 'Internal Server Error'
+        })
+    }
+}
+export default is_property_of
