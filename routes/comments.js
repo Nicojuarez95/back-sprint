4e672ab52aac087f7067d2e795e4af3272963964
salomoncjl <115399253+salomoncjl@users.noreply.github.com>
2023-04-02 23:56:17 -0300

    M 16 dev (#20)
    * create comment
    
    * comments agregado a index router
    
    * Crear comentarios terminado
    
    * Read de comentarios y enrutado
    
    * update terminado
    
    * destroy y ruta terminada
    
    * ruta fix

--- routes/comments.js
+++ routes/comments.js
@@ -6,17 +6,22 @@ import validator from '../middlewares/validator.js'
 import passport from '../middlewares/passport.js'
 import updateComment from '../controllers/comments/update.js'
 import is_property_of from '../middlewares/comments/is_property_of.js'
+import deleteComment from '../controllers/comments/destroy.js'
+
 
 
 let router = express.Router();
 
 const { create } = createComment;
 const { all_from_chapter } = all_from_chapters
-
 const { update } = updateComment
+const { destroy } = deleteComment
+
 
 router.post('/', passport.authenticate("jwt", {session: false}), validator(createSchema), create);
 router.get('/', passport.authenticate("jwt", {session: false}), all_from_chapter);
 router.put('/:id', passport.authenticate("jwt", {session: false}), validator(createSchema), is_property_of, update)
+router.delete('/:id', passport.authenticate("jwt", {session: false}), is_property_of, destroy)
+
 
 export default router
